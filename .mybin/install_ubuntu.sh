ab112f3eb696fd9c0e8bbb1cf84df729d1252744
gpetrousov <petrousov@gmail.com>
2020-02-21 12:02:09 +0100

    Added bootstrap script for a fresh Ubuntu box

---
+++ .mybin/install_ubuntu.sh
@@ -0,0 +1,23 @@
+# Ioannis Petrousov
+# petrousov@gmail.com
+
+# This script will install mydot files on a fresh Ubuntu system.
+
+# Requirements
+apt-get update && apt-get -y install\
+		neovim\
+		git\
+
+# Create mydot command
+alias mydot='/usr/bin/git --git-dir=$HOME/.mydot/ --work-tree=$HOME'
+
+# Clone the mydot project
+git clone --bare git@github.com:gpetrousov/mydot.git $HOME/.mydot
+
+# Checkout the actual content from the bare repository to your $HOME
+`mydot checkout`
+
+# Install fresh neovim plugins using Plug
+alias vim='nvim'
+vim -E .config/nvim/plug.vim +PlugInstall +qall
+
