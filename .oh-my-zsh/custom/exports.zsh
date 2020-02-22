46d0191abd8fbf141a47e9f4e9af3291938b8745
gpetrousov <petrousov@gmail.com>
2020-02-22 17:12:08 +0100

    Fix git aliases
    Added zsh and oh-my-zsh dotfiles

---
+++ .oh-my-zsh/custom/exports.zsh
@@ -0,0 +1,20 @@
+# ==================================== #
+# Exports
+# ==================================== #
+
+export HOMEBREW_GITHUB_API_TOKEN=1cf81cac8be33d1727af36a67c506ac5842f595f
+
+# This fixes the warning I got from pip3 when installing awscli
+export PATH=$PATH:$HOME/Library/Python/3.7/bin:$HOME/Library/Python/2.7/bin
+
+# This includes openvpn binary
+export PATH=$PATH:/usr/local/Cellar/openvpn/2.4.7/sbin
+
+# This fixes the errors I got from perl when using the Ack plugin in vim
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# Export GOPATH
+export GOPATH=$HOME/go
+
+# ==================================== #
