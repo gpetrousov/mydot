8df53a606d59ff3521790cd9b6ea111772999f0c
gpetrousov <petrousov@gmail.com>
2020-02-25 13:57:53 +0100

    Split corporate preferences in separate custom file

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -6,22 +6,12 @@
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# Always use neovim
 alias vim='nvim'
-#
-# Mirabeau Apache
-alias prdweb1='ssh -i ~/.ssh/maxeda_vpn ipetrousov@10.227.136.10'
-# Maxeda apache (Apache is no more;Long live webedge)
-#alias httpd01prd01='ssh -i ~/.ssh/maxeda_vpn ipetrousov@172.31.131.113'
 
 # Always use colordiff
 alias diff="colordiff"
-
-# Maxeda AWS prodnotprod account
-alias awsmax-prodnotprod='unset AWS_PROFILE; aws-google-auth -p awsmax-prodnotprod ; export AWS_PROFILE=awsmax-prodnotprod'
 
 # Tracking for mydotfiles
 alias mydot='/usr/bin/git --git-dir=$HOME/.mydot/ --work-tree=$HOME' >> $HOME/.zshrc
