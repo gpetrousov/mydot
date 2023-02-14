ed179befd3b133c5149a1b0f886c5c7c8270dbb1
gpetrousov <petrousov@gmail.com>
2023-02-14 10:38:54 +0100

    Set scale to 1 in laptop display only script since dpi is set in Xresources

--- .screenlayout/laptop_only.sh
+++ .screenlayout/laptop_only.sh
@@ -5,16 +5,13 @@
 # Author: Ioannis Petrousov
 # Date: 26-11-2022
 
-external_displays=$(xrandr | cut -f 1 -d ' ' | grep -v eDP1)
-set_display="xrandr --output eDP1 \
-    --primary --mode 2256x1504 \
-    --pos 0x0 --rotate normal \
-    --scale 1 "
 
-for ed in $external_displays
-do
-  set_display="${set_display} --output ${ed} --off "
-done
+# Settings for laptop display
+set_display="xrandr --output eDP1 \
+  --primary --mode 2256x1504 \
+  --pos 0x0 --rotate normal \
+  --scale 1x1"
+
 echo ${set_display}
 
 eval "$set_display"
