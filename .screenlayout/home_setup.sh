018b7ab8765863864750fddc62a9eb7bd34d3d16
gpetrousov <petrousov@gmail.com>
2023-11-08 11:42:34 +0100

    Update screenlayout script

--- .screenlayout/home_setup.sh
+++ .screenlayout/home_setup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 2256x1504 --pos 0x0 --rotate normal --output DP1 --primary --mode 3440x1440 --pos 2256x0 --rotate normal --scale 1.2 --output DP2 --off --output DP3 --off --output DP4 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --mode 2256x1504 --pos 0x0 --rotate normal --output DP1 --primary --mode 3440x1440 --pos 2256x0 --rotate normal --scale 1 --output DP2 --off --output DP3 --off --output DP4 --off --output VIRTUAL1 --off
---
+++ .screenlayout/zen_3_screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 2256x1504 --pos 0x1080 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --mode 1024x768 --pos 1920x312 --rotate normal --output DP4 --off --output VIRTUAL1 --off
